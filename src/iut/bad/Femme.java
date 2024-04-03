a452446847b1ecf47789c90141ccbc76643f7b4a
Tommymiza <tommymiza20@gmail.com>
2024-04-03 08:40:46 +0300

    feat-surcharge de la fonction toString dans les classes enfants

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -1,5 +1,19 @@
 package iut.bad;
 
-public class Femme extends Humain{
-	
+public class Femme extends Humain {
+    // Constructeur par défaut
+    public Femme() {
+        super(); // Appel du constructeur par défaut de la classe parent Humain
+    }
+
+    // Constructeur avec paramètres
+    public Femme(String nom, String prenom, int age) {
+        super(nom, prenom, age); // Appel du constructeur avec paramètres de la classe parent Humain
+    }
+    
+    // Surcharge de la méthode toString() pour les femmes
+    @Override
+    public String toString() {
+        return "C'est une femme\n" + super.toString(); // Appel de la méthode toString() de la classe parent Humain
+    }
 }
--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -1,6 +1,19 @@
 package iut.bad;
 
-public class Homme extends Humain{
-    // Champs
-  
+public class Homme extends Humain {
+    // Constructeur par défaut
+    public Homme() {
+        super(); // Appel du constructeur par défaut de la classe parent Humain
+    }
+
+    // Constructeur avec paramètres
+    public Homme(String nom, String prenom, int age) {
+        super(nom, prenom, age); // Appel du constructeur avec paramètres de la classe parent Humain
+    }
+    
+    // Surcharge de la méthode toString() pour les hommes
+    @Override
+    public String toString() {
+        return "C'est un homme\n" + super.toString(); // Appel de la méthode toString() de la classe parent Humain
+    }
 }
