f32ec90baf52686ea75a41dab1fb451011b1f509
Tommymiza <tommymiza20@gmail.com>
2024-04-03 08:28:46 +0300

    refact-deplacement des attributs de la classe enfant vers la classe parent

--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -2,23 +2,5 @@ package iut.bad;
 
 public class Homme extends Humain{
     // Champs
-    private String nom;
-    private String prenom;
-    private int age;
-
-    
-    public Homme() {
-        this.nom = "";
-        this.prenom = "";
-        this.age = 0;
-    }
-
-    
-    public Homme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-    }
-
-   
+  
 }
--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -1,5 +1,7 @@
 package iut.bad;
 
 public class Humain {
-
+	 private String nom;
+	 private String prenom;
+	 private int age;
 }
