85a196065ab4dfc20dd7d32b008e9c57ff17aa12
Tommymiza <tommymiza20@gmail.com>
2024-04-03 08:32:04 +0300

    feat-creation de la methode details dans humain

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -1,7 +1,58 @@
 package iut.bad;
 
 public class Humain {
-	 private String nom;
-	 private String prenom;
-	 private int age;
+    private String nom;
+    private String prenom;
+    private int age;
+    
+    public Humain() {
+        this.nom = "";
+        this.prenom = "";
+        this.age = 0;
+    }
+
+    
+    public Humain(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    
+    
+    
+    public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public void details() {
+        System.out.println("Nom: " + nom);
+        System.out.println("Prénom: " + prenom);
+        System.out.println("Âge: " + age);
+    }
 }
